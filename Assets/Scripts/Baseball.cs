53f84f3aad05f88b094cf15a1bfd690d6848090e
BogDog4 <Bramanofficial@gmail.com>
2023-12-17 15:10:51 -0500

    baseball scripts done

--- Assets/Scripts/Baseball.cs
+++ Assets/Scripts/Baseball.cs
@@ -2,23 +2,33 @@ using UnityEngine;
 
 public class Baseball : MonoBehaviour
 {
-    public float speed = 5f; // Adjust the speed as needed
-    public float despawnTime = 5f; // Adjust the despawn time as needed
+    public float speed = 5f;
+    public float despawnTime = 5f;
+    public float despawnDistance = 10f;
     private Rigidbody rb;
     private float spawnTime;
+    private GameObject player;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         spawnTime = Time.time;
+        player = GameObject.FindGameObjectWithTag("Player");
+        
         // Move the baseball forward
         rb.velocity = transform.forward * speed;
     }
 
     void Update()
     {
-        // Check if the baseball should be despawned
+        // Check if the baseball should be despawned based on time
         if (Time.time - spawnTime > despawnTime)
+        {
+            Destroy(gameObject);
+        }
+
+        // should i be despawned based on distance from the player
+        if (player != null && Vector3.Distance(transform.position, player.transform.position) > despawnDistance)
         {
             Destroy(gameObject);
         }
@@ -29,7 +39,7 @@ public class Baseball : MonoBehaviour
         // Check if the baseball collides with something
         if (collision.gameObject.CompareTag("Bat"))
         {
-            // Calculate the new velocity when hit by the bat
+            // Calculate a new velocity when hit by the bat
             Vector3 newVelocity = (collision.transform.forward + Vector3.up).normalized * speed * 2f;
 
             // Apply the new velocity to the baseball
@@ -40,7 +50,7 @@ public class Baseball : MonoBehaviour
         }
         else
         {
-            // Stop the baseball when it hits something else
+            // Stop the baseball when it hits something that isnt a bat
             rb.velocity = Vector3.zero;
         }
     }
