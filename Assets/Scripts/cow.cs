a144ac2b5aa64e3ade382d354f78ab793264f6e3
BogDog4 <Bramanofficial@gmail.com>
2023-12-18 07:54:11 -0500

    h

--- Assets/Scripts/cow.cs
+++ Assets/Scripts/cow.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class cow : MonoBehaviour
 {
     public GameObject baseballPrefab;
-    public float SpawnInterval = 1f;  // time between throws
-    public float throwRange = 10f;   // maximum distance to throw baseballs
-    public Transform player;         // reference to the player object
+    public float SpawnInterval = 1f;  
+    public float throwRange = 10f;  
+    public Transform player;         
 
     private float nextSpawnTime;
 
@@ -36,20 +36,16 @@ public class cow : MonoBehaviour
         // Instantiate the baseball prefab at the spawner's position
         GameObject newBaseball = Instantiate(baseballPrefab, transform.position, Quaternion.identity);
 
-        // Get the rotation of the cow
+        // Get the rotation of the cow(gun)
         Quaternion cowRotation = transform.rotation;
 
-        // Set the rotation of the new baseball to match the cow's rotation
+        // Set the rotation of baseball to match rotation
         newBaseball.transform.rotation = cowRotation;
 
-        // You may need to adjust the above code depending on how your cow model is set up.
-        // If your cow model is facing a different direction initially, you might need to modify the rotation.
-
-        // Access the Rigidbody (if your baseball has one) and apply force in the forward direction
+        // Access the Rigidbody and apply force in the forward direction
         Rigidbody rb = newBaseball.GetComponent<Rigidbody>();
         if (rb != null)
         {
-            // You can adjust the force value based on your requirements
             float force = 10f;
             rb.AddForce(newBaseball.transform.forward * force, ForceMode.Impulse);
         }
